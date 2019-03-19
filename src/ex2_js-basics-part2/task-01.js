8ebeacfeb8f860972dff9852c87140ccfedb9be8
Anastasia <43821164+AnaBelochka@users.noreply.github.com>
2019-03-19 19:07:03 +0300

    Delete task-01.js
    Delete unnesessary file

--- src/ex2_js-basics-part2/task-01.js
+++
@@ -1,8 +0,0 @@
-function stringOrNumber (variable){
-    var typeOfVariable = typeof variable;
-    if (isNaN(variable)) return undefined;
-    if (typeOfVariable === 'number' || typeOfVariable === 'string') return typeOfVariable;
-    return undefined;
-}
-
-module.exports = stringOrNumber;
