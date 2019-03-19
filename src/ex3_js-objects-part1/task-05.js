cb029520c7f2c74150cd947f11f2a422a2675b45
Anastasia <43821164+AnaBelochka@users.noreply.github.com>
2019-03-19 17:20:53 +0300

    Delete task-05.js
    Delete unnecessary file

--- src/ex3_js-objects-part1/task-05.js
+++
@@ -1,9 +0,0 @@
-function cloneObject (object) {
-    var newObject = {};
-    for (const key in object) {
-        newObject[key] = object.key;
-    }
-    return newObject;
-}
-
-module.exports = cloneObject;
