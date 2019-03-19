47a78670d615d6690074b1a75df961e2ef0bfb04
Анастасия Цой <tsoi.nastya@gmail.com>
2019-03-19 21:57:52 +0300

    I completed all tasks for exercise 3 and created functions: functions which are working with properties of object (task 1-4) and functions which make clone and deep clone of object

---
+++ src/ex3_js-objects-part1/task-02.js
@@ -0,0 +1,9 @@
+function returnObjectInfo (object) {
+    for (const key in object) {
+        if (object.hasOwnProperty(key)) {
+            console.log(key+object.key);            
+        }
+    }
+}
+
+module.exports = returnObjectInfo;
