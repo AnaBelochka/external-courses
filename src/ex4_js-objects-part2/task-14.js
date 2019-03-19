d96bcd9f9bd214d3c14214a6431587117d0e25f9
Anastasia <43821164+AnaBelochka@users.noreply.github.com>
2019-03-19 22:05:29 +0300

    Delete task-14.js
    Sorry for this mistakes. Delete unnecessary file.

--- src/ex4_js-objects-part2/task-14.js
+++
@@ -1,9 +0,0 @@
-function randomValueInInterval (min, max) {
-    var value = Math.round(Math.random() * 10);
-    while (!(value <= max && value >= min )) {
-        value = Math.round(Math.random() * 10);    
-    }
-    return value;
-}
-
-module.exports = randomValueInInterval;
