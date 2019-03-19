b8aaf4b7c9ad4a3e4926746c22b0e5d1e8cb59f3
Anastasia <43821164+AnaBelochka@users.noreply.github.com>
2019-03-19 17:20:40 +0300

    Delete task-04.js
    Delete unnecessary file

--- src/ex3_js-objects-part1/task-04.js
+++
@@ -1,8 +0,0 @@
-function findAndCreateProperty (string, object) {
-    if (!(string in object)) {
-        object[string] = "new";    
-    }
-    return object;
-}
-
-module.exports = findAndCreateProperty;
